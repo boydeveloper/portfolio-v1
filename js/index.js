25e3f325a6b756415176238dcbf48c6aea52aa2f
boy developer <daniel.onazi116@gmail.com>
2024-06-12 16:44:36 +0100

    tweaks

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-const sections = document.querySelectorAll(".Project");
+// const sections = document.querySelectorAll(".Project");
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("scroll-animation");
-      } else {
-        entry.target.classList.remove("scroll-animation");
-      }
-    });
-  },
-  {
-    threshold: 0.8,
-  }
-);
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("scroll-animation");
+//       } else {
+//         entry.target.classList.remove("scroll-animation");
+//       }
+//     });
+//   },
+//   {
+//     threshold: 0.8,
+//   }
+// );
 
-for (let i = 0; i < sections.length; i++) {
-  observer.observe(sections[i]);
-}
+// for (let i = 0; i < sections.length; i++) {
+//   observer.observe(sections[i]);
+// }
